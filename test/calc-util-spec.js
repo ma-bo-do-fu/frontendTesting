687fc24ce4bf74050a668906262f1db03f252cbe
to-morita <sby02773@gmail.com>
2016-09-26 14:42:39 +0900

    add mocha

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,8 +1,12 @@
-console.info('add関数のテスト');
-console.info('1+2は3である');
-
-if (add(1, 2) === 3) {
-    console.info('成功')
-} else {
-    console.error('失敗')
-}
+describe('add関数のテスト', function() {
+  it('1+2は3である', function() {
+    if (add(1, 2) === 3) {} else {
+      throw new Error('失敗');
+    }
+  });
+  it('1+2は4である', function() {
+    if (add(1, 2) === 4) {} else {
+      throw new Error('失敗');
+    }
+  });
+});
