e5225089f4aa6f226c0896d4140ef11c65be2f41
to-morita <sby02773@gmail.com>
2016-09-26 14:45:51 +0900

    use mocha with Node.js

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,11 +1,13 @@
+var calcUtil = require('../src/calc-util.js');
+
 describe('add関数のテスト', function() {
   it('1+2は3である', function() {
-    if (add(1, 2) === 3) {} else {
+    if (calcUtil.add(1, 2) === 3) {} else {
       throw new Error('失敗');
     }
   });
   it('1+2は4である', function() {
-    if (add(1, 2) === 4) {} else {
+    if (calcUtil.add(1, 2) === 4) {} else {
       throw new Error('失敗');
     }
   });
