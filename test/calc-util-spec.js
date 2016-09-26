8be65e0e811c6c61cb25dc257dbddc33f526be13
to-morita <sby02773@gmail.com>
2016-09-26 14:55:06 +0900

    use assert

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,14 +1,11 @@
 var calcUtil = require('../src/calc-util.js');
+var assert = require('assert');
 
 describe('add関数のテスト', function() {
   it('1+2は3である', function() {
-    if (calcUtil.add(1, 2) === 3) {} else {
-      throw new Error('失敗');
-    }
+    assert(calcUtil.add(1, 2) === 3);
   });
   it('1+2は4である', function() {
-    if (calcUtil.add(1, 2) === 4) {} else {
-      throw new Error('失敗');
-    }
+    assert(calcUtil.add(1, 2) === 4);
   });
 });
